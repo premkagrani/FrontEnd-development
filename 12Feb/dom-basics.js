d608a84670d38ae5840f8dda16476ef476043a76
michaelscof <pkagrani@gmail.com>
2019-02-12 15:11:36 +0530

    Event bubbling

--- 12Feb/dom-basics.js
+++ 12Feb/dom-basics.js
@@ -1,5 +1,15 @@
 //Event handler for button click
-function save(){
-    var name=document.getElementById("name");
-    console.log('clicked',name.value);
+function save(event) {
+    //var name = document.getElementById("name");
+    //var nameDomElement=document.getElementsByTagName('input')[0];
+    var nameDomElement=document.getElementsByName('firstName')[0];
+    var name=document .getElementById('name');//selects first tag matching the criteria
+    console.log(name.value)
+    console.log('clicked',nameDomElement.value)
+    //console.log('clicked', name.value);
+    console.log(event);
+    event.stopPropagation();//to stop event from bubbling up
+}
+function foo() {
+    console.log('div click handler');
 }
